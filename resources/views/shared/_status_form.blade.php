23f07da6f10704d72e14c579fbf5081e001fec38
祝垚杰 <zyjqjnh@126.com>
2018-12-27 08:43:37 +0000

    首页微博列表

--- resources/views/shared/_status_form.blade.php
+++ resources/views/shared/_status_form.blade.php
@@ -2,7 +2,7 @@
     @include('shared._errors')
     {{ csrf_field() }}
     <textarea class="form-control" rows="3" placeholder="聊聊新鲜事儿..." name="content">{{ old('content') }}</textarea>
-    <div>
+    <div class="text-right">
         <button type="submit" class="btn btn-primary mt-3">发布</button>
     </div>
 </form>
