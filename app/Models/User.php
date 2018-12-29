d6008b03c698d5cd53ef79ea118d68454f24a9d3
祝垚杰 <zyjqjnh@126.com>
2018-12-29 06:23:43 +0000

    关注按钮

--- app/Models/User.php
+++ app/Models/User.php
@@ -67,7 +67,7 @@ class User extends Authenticatable
         if (!is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
-        $this->followers()->sync($user_ids, false);
+        $this->followings()->sync($user_ids, false);
     }
 
     public function unfollow($user_ids)
@@ -75,11 +75,11 @@ class User extends Authenticatable
         if (!is_array($user_ids)) {
             $user_ids = compact('user_ids');
         }
-        $this->followers()->detach($user_ids);
+        $this->followings()->detach($user_ids);
     }
 
     public function isFollowing($user_id)
     {
-        return $this->followings()->contains($user_id);
+        return $this->followings->contains($user_id);
     }
 }
